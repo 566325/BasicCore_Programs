3378f1eae7ba0b0a44a7c94ddbd5abc35377fc65
566325 <nethisireesha288@gmail.com>
2023-09-06 18:41:35 +0530

    [Sireesha]: Added Stopwatch program

---
+++ StopWatchClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class StopWatch
+    {
+        private string Elapsed;
+
+        public static void StartAndEndStopWatch()
+        {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                Console.WriteLine(i);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Time Elapsed:" + stopWatch.Elapsed);
+
+
+        }
+
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
